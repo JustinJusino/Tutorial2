6703ec67a6cce84b8aa4c8fb3607914b0ff83339
JustinJusino <justinjusino@gmail.com>
2020-10-25 16:22:19 -0400

    Challenge commit

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyScript : MonoBehaviour
+{
+    private Rigidbody2D rd2d; 
+    float speed = 3f;
+    float height = 1.5f;
+
+    Vector3 pos;
+
+    void Start()
+    {
+         rd2d = GetComponent<Rigidbody2D>();
+         pos = transform.position;
+    }
+void Update()
+{
+
+//calculate what the new Y position will be
+float newY = Mathf.Sin(Time.time * speed) * height + pos.y;
+//set the object's Y to the new calculated Y
+transform.position = new Vector3(transform.position.x, newY, transform.position.z) ;
+}
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,13 +9,31 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D rd2d; 
     public float speed;
     public Text score;
+    public Text winText;
     private int scoreValue = 0;
+    public Text livesText;
+    private int livesValue = 3;
+    public Text loseText;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
+    Animator anim;
+    Vector3 playerScale;
+    float playerScaleX;
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        livesText.text = livesValue.ToString();
+        anim = GetComponent<Animator>();
+        playerScale = transform.localScale;
+        playerScaleX = playerScale.x;
     }
 
     // Update is called once per frame
@@ -23,9 +41,8 @@ public class PlayerScript : MonoBehaviour
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float verMovement = Input.GetAxis("Vertical");
-
         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed)); 
-        
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -35,22 +52,113 @@ public class PlayerScript : MonoBehaviour
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
         }
+    
+        if(scoreValue == 4)
+        {
+            scoreValue += 1;
+            livesValue /= livesValue;
+            livesValue += 2;
+            livesText.text = livesValue.ToString();
+            transform.position = new Vector2(58.8f, 0.7f);
+        }
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            livesText.text = livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+        if (livesValue == 0)
+        {
+            loseText.text = ("You Lost! Exit the Game using 'Esc'");
+            Destroy(GameObject.FindWithTag("Player"));
+        }
+        if (scoreValue == 9)
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.PlayOneShot(musicClipTwo);
+            musicSource.loop = false;
+            winText.text = ("You Won ! Exit the Game using 'Esc'");
+        }
     }
+
     void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.collider.tag == "Ground")
+        if(collision.collider.tag == "Ground" && isOnGround)
         {
             if(Input.GetKey(KeyCode.W))
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+            
         }
     }
     void Update()
-    {
+    {   
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
+            if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+              anim.SetInteger("State", 0); 
+        }
+         if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetInteger("State", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+              anim.SetInteger("State", 0); 
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            playerScale.x = -playerScaleX;
+        }
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            playerScale.x = playerScaleX;
+        }
+        transform.localScale = playerScale;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("State", 2);
+        }
+         if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+              anim.SetInteger("State", 0); 
+        }
+             if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+              anim.SetInteger("State", 0); 
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 3);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+             anim.SetInteger("State", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetInteger("State", 3);
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+             anim.SetInteger("State", 0);
+        }
     }
 }
+
